397991f3c25924d11daeb6ecf6b06a25aa47ff04
Logan Woolf <loganwoolf@gmail.com>
2021-07-25 15:34:17 -0600

    create game function

--- script.js
+++ script.js
@@ -7,3 +7,27 @@ function computerPlay() {
    return weapons[randomNumber];
 }
 
+function playRound(playerSelection, computerSelection) {
+   //handle tie
+   if (playerSelection === computerSelection) {
+      return `Tie, both players chose ${playerSelection}.`
+   } else if (computerSelection === 'Rock') {
+      if (playerSelection === 'Paper') {
+         return 'You Win, Paper beats Rock.'
+      } else {
+         return 'You Lose, Rock beats Scissors.'
+      }
+   } else if (computerSelection === 'Paper') {
+      if (playerSelection === 'Scissors') {
+         return 'You Win, Scissors beats Paper.'
+      } else {
+         return 'You Lose, Paper beats Rock.'
+      }
+   } else if (computerSelection === 'Scissors') {
+      if (playerSelection === 'Rock') {
+         return 'You Win, Rock beats Scissors.'
+      } else {
+         return 'You Lose, Scissors beats Paper.'
+      }
+   }
+}
